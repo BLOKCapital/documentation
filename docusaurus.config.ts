3862847fa431cb8715313cc10f18e98ec636de7e
Limbachiya Chintan <124679683+0xChintan@users.noreply.github.com>
2025-02-28 14:37:31 +0530

    update the documentation components

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,65 +1,65 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'BLOK Capital',
-  tagline: 'BLOK Capital',
-  favicon: 'img/blokclogo.svg',
+  title: "BLOK Capital",
+  tagline: "BLOK Capital",
+  favicon: "img/blokclogo.svg",
 
   // Set the production url of your site here
-  url: 'https://.pages.dev',
+  url: "https://.pages.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -67,60 +67,59 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'BLOK Capital',
-      
+      title: "BLOK Capital",
+
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/blokclogo.svg',
-        href: 'https://blokcapital.io/',
+        alt: "My Site Logo",
+        src: "img/blokclogo.svg",
+        href: "/",
       },
-      // items: [
-      //   {
-      //     type: 'docSidebar',
-      //     sidebarId: 'tutorialSidebar',
-      //     position: 'left',
-      //     label: 'Documentation',
-      //   },
-      //   {
-      //     href: 'https://github.com/facebook/docusaurus',
-      //     label: 'GitHub',
-      //     position: 'right',
-      //   },
-      // ],
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Documentation",
+        },
+        {
+          href: 'https://github.com/BLOKCapital/documentation',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
-
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Telegram',
-              href: 'https://t.me/BLOKCapital',
+              label: "Telegram",
+              href: "https://t.me/BLOKCapital",
             },
             {
-              label: 'X',
-              href: 'https://x.com/blok_cap',
+              label: "X",
+              href: "https://x.com/blok_cap",
             },
             {
-              label: 'Farcaster',
-              href: 'https://warpcast.com/blokc',
+              label: "Farcaster",
+              href: "https://warpcast.com/blokc",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Website',
-              href: 'https://blokcapital.io/',
+              label: "Website",
+              href: "https://blokcapital.io/",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/BLOKCapital',
+              label: "GitHub",
+              href: "https://github.com/BLOKCapital",
             },
           ],
         },

1723fedaafdd76a895733db9ba0c5ae2d13902af
heymustafa02 <heymustafa.work@gmail.com>
2025-07-31 16:18:04 +0530

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,12 +1,6 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
-import translations from "./src/data/translations.json";
-
-// Helper to get translation object for current locale
-function getT(locale: string) {
-  return translations[locale];
-}
 
 const config: Config = {
   title: "BLOK Capital",
@@ -33,7 +27,7 @@ const config: Config = {
         docs: {
           sidebarPath: require.resolve("./sidebar/sidebars.ts"),
           routeBasePath: "/",
-          editUrl: "https://github.com/BLOKCapital/documentation/issues",
+          editUrl: 'https://github.com/BLOKCapital/documentation/issues',
         },
         blog: {
           showReadingTime: true,
@@ -64,7 +58,7 @@ const config: Config = {
         path: "educ-docs",
         routeBasePath: "educ",
         sidebarPath: require.resolve("./sidebar/sidebars-educ.ts"),
-        editUrl: "https://github.com/BLOKCapital/documentation/issues",
+        editUrl: 'https://github.com/BLOKCapital/documentation/issues',
       },
     ],
     [
@@ -74,7 +68,7 @@ const config: Config = {
         path: "builders-docs",
         routeBasePath: "builders",
         sidebarPath: require.resolve("./sidebar/sidebars-builders.ts"),
-        editUrl: "https://github.com/BLOKCapital/documentation/issues",
+        editUrl: 'https://github.com/BLOKCapital/documentation/issues',
       },
     ],
     [
@@ -84,104 +78,95 @@ const config: Config = {
         path: "resources-docs",
         routeBasePath: "resources",
         sidebarPath: require.resolve("./sidebar/sidebars-resources.ts"),
-        editUrl: "https://github.com/BLOKCapital/documentation/issues",
+        editUrl: 'https://github.com/BLOKCapital/documentation/issues',
       },
     ],
   ],
 
   themeConfig: {
     image: "img/docusaurus-social-card.jpg",
-    navbar: (function () {
-      // Docusaurus only supports static config, so we use the default locale for build-time
-      const locale = process.env.DOCUSAURUS_CURRENT_LOCALE || "en";
-      const t = getT(locale).navbar;
-      return {
-        title: "BLOK Capital",
-        logo: {
-          alt: "BLOK Capital Logo",
-          src: "img/blokclogo.svg",
-          href: "/",
+    navbar: {
+      title: "BLOK Capital",
+      logo: {
+        alt: "BLOK Capital Logo",
+        src: "img/blokclogo.svg",
+        href: "/",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Introduction",
         },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: t.introduction,
-          },
-          {
-            to: "/educ/intro",
-            label: t.concepts,
-            position: "left",
-          },
-          {
-            to: "/builders/intro",
-            label: t.builders,
-            position: "left",
-          },
-          {
-            to: "/resources/intro",
-            label: t.resources,
-            position: "left",
-          },
-          {
-            type: "localeDropdown",
-            position: "right",
-            dropdownItemsAfter: [
-              {
-                label: t.helpTranslate,
-                href: "https://github.com/BLOKCapital/documentation/issues",
-              },
-            ],
-          },
-          {
-            label: t.whitepaper,
-            position: "right",
-            href: "https://docsend.com/view/qqzdvsv2q47g6t9y",
-          },
-        ],
-      };
-    })(),
-    footer: (function () {
-      const locale = process.env.DOCUSAURUS_CURRENT_LOCALE || "en";
-      const t = getT(locale).footer;
-      return {
-        style: "dark",
-        links: [
-          {
-            title: t.community,
-            items: [
-              {
-                label: t.telegram,
-                href: "https://t.me/BLOKCapital",
-              },
-              {
-                label: t.twitter,
-                href: "https://x.com/blok_cap",
-              },
-              {
-                label: t.farcaster,
-                href: "https://warpcast.com/blokc",
-              },
-            ],
-          },
-          {
-            title: t.more,
-            items: [
-              {
-                label: t.website,
-                href: "https://blokcapital.io/",
-              },
-              {
-                label: t.github,
-                href: "https://github.com/BLOKCapital",
-              },
-            ],
-          },
-        ],
-        copyright: t.copyright.replace("{year}", `${new Date().getFullYear()}`),
-      };
-    })(),
+        {
+          to: "/educ/intro",
+          label: "Concepts",
+          position: "left",
+        },
+        {
+        to: '/builders/intro',
+        label: 'Builders',
+        position: 'left',
+      },
+      {
+        to: '/resources/intro',
+        label: 'Resources',
+        position: 'left',
+      },
+        {
+          type: "localeDropdown",
+          position: "right",
+          dropdownItemsAfter: [
+            {
+              label: "Help us translate",
+              href: "https://github.com/BLOKCapital/documentation/issues", // Replace with your translation platform link
+            },
+          ],
+        },
+        {
+          label: "Whitepaper",
+          position: "right",
+          href: "https://docsend.com/view/qqzdvsv2q47g6t9y",
+        }
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Telegram",
+              href: "https://t.me/BLOKCapital",
+            },
+            {
+              label: "X (Twitter)",
+              href: "https://x.com/blok_cap",
+            },
+            {
+              label: "Farcaster",
+              href: "https://warpcast.com/blokc",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Website",
+              href: "https://blokcapital.io/",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/BLOKCapital",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} BLOK Capital DAO LLC. All rights reserved.`,
+    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
